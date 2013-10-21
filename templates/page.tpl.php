03081c210b1d6d5cfaba67e3003813d88da5b19a
Carles Bellver <carles.bellver@gmail.com>
2013-10-21 08:49:59 +0200

    More fixes...

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -68,11 +68,12 @@
   <header id="header" class="clearfix">
     <?php if (theme_get_setting('image_logo','cent_drupal7_responsive')): ?>
       <?php if ($logo): ?>
-		<?php if (theme_get_setting('image_uji_logo','cent_drupal7_responsive')): ?>
-		  <a href="http://www.uji.es" title="Universitat Jaume I"><img src="http://cent.uji.es/pub/sites/all/themes/cent_drupal7_responsive/Logo-UJI-50.png" alt="Universitat Jaume I" /></a>
-		<?php endif; ?>
-      	<div id="site-logo"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a></div>
+      	<div id="site-logo">
+      	<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+        <?php if (theme_get_setting('image_uji_logo','cent_drupal7_responsive')): ?>
+          <a href="http://www.uji.es" title="Universitat Jaume I"><img src="http://cent.uji.es/pub/sites/all/themes/cent_drupal7_responsive/Logo-UJI-50.png" alt="Universitat Jaume I" /></a>
+        <?php endif; ?>
+      	</div>
       <?php endif; ?>
     <?php else: ?>
       <hgroup id="site-name-wrap">
